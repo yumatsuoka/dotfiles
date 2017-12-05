9a4541c9a9f0679a0ce91dcd3d0fba39f83f0553
YumaMatsuoka <a.iebako.yuma@gmail.com>
2017-12-05 16:10:21 -0500

    Update pyenv_install. add "pip install cudnnenv"

--- scripts/conda_install.sh
+++ scripts/conda_install.sh
@@ -5,5 +5,7 @@ pip list --outdated --format=legacy | awk '{print $1}' | xargs pip install -U
 
 pip install tqdm
 pip install seaborn
-# pip install cupy
+pip install cudnnenv
+cudnnenv install v5-cuda8
+pip install cupy
 pip install chainer --no-cache-dir
