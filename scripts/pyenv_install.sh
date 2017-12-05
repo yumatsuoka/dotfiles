d7d53063ed37251fb98c467e8a60c8fb91a9dbce
YumaMatsuoka <a.iebako.yuma@gmail.com>
2017-12-05 16:08:49 -0500

    pyenvでpip使った後のpyenv rehashを自動で行ってくれるプラグインの追記

--- scripts/pyenv_install.sh
+++ scripts/pyenv_install.sh
@@ -3,6 +3,7 @@
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev
 
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/yyuu/pyenv-pip-rehash.git ~/.pyenv/plugins/pyenv-pip-rehash
 
 source .bashrc
 pyenv install --list
